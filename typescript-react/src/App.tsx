b423025746da17304dd54b4b52d79108d8857bdc
NexZoa-Naganandhini <naganandhini.nexzoatw@gmail.com>
2023-03-11 22:40:56 +0530

    Project initial setup

---
+++ typescript-react/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+    return (
+        <div className="App">Simple Weather Application using typescript</div>
+    );
+};
+
+export default App;
